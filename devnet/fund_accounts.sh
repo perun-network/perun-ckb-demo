33db9c32e67026f2064cb7a5c671fbffbf14cdbb
Norbert Dzikowski <norbert@perun.network>
2023-06-15 13:41:26 +0200

    devnet: Sleep between funding transactions

--- devnet/fund_accounts.sh
+++ devnet/fund_accounts.sh
@@ -5,4 +5,5 @@ alice=$(cat accounts/alice.txt | awk '/testnet/ && !found {print $2; found=1}')
 bob=$(cat accounts/bob.txt | awk '/testnet/ && !found {print $2; found=1}')
 
 echo -e '\n' | ckb-cli wallet transfer --from-account $genesis --to-address $alice --capacity 10000000000
+sleep 2.0
 echo -e '\n' | ckb-cli wallet transfer --from-account $genesis --to-address $bob --capacity 10000000000
