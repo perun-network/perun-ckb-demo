f7426c579e0b9e5abc4f498dfbc3654d05748537
Norbert Dzikowski <norbert@perun.network>
2023-06-19 12:33:33 +0200

    devnet: Fetch system-scripts when devnet setup finished

--- devnet/deploy_contracts.sh
+++ devnet/deploy_contracts.sh
@@ -2,6 +2,8 @@
 
 ACCOUNTS_DIR="accounts"
 PERUN_CONTRACTS_DIR="contracts"
+SYSTEM_SCRIPTS_DIR="system_scripts"
+DEVNET_DIR="$PWD"
 
 genesis=$(cat $ACCOUNTS_DIR/genesis-2.txt | awk '/testnet/ { count++; if (count == 2) print $2}')
 
@@ -19,3 +21,14 @@ expect "Password:"
 send "\r"
 expect eof
 EOF
+
+# Fetch default contracts:
+cd $DEVNET_DIR
+
+if [ -d "$SYSTEM_SCRIPTS_DIR" ]; then
+  rm -rf "$SYSTEM_SCRIPTS_DIR"
+fi
+
+mkdir -p "$SYSTEM_SCRIPTS_DIR"
+## yq/jq will interpret the code_hash and tx_hash as numbers, so we need to wrap them in quotes.
+ckb-cli util genesis-scripts | sed 's/code_hash: \(.*\)/code_hash: \"\1\"/; s/tx_hash: \(.*\)/tx_hash: \"\1\"/' | yq . > "$SYSTEM_SCRIPTS_DIR/genesis-scripts.json"
