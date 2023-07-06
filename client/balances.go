9851070866de230922523503f07c7fb67bf9c0b5
Norbert Dzikowski <norbert@perun.network>
2023-07-06 20:18:53 +0200

    client: Show CKByte and SUDTBalances in interface

--- client/balances.go
+++ client/balances.go
@@ -2,15 +2,31 @@ package client
 
 import (
 	"context"
-	"github.com/nervosnetwork/ckb-sdk-go/v2/indexer"
-	"github.com/nervosnetwork/ckb-sdk-go/v2/types"
+	"encoding/binary"
+	"fmt"
 	"log"
 	"math"
 	"math/big"
-	"perun.network/perun-ckb-backend/wallet/address"
 	"strconv"
 	"time"
+
+	"github.com/nervosnetwork/ckb-sdk-go/v2/indexer"
+	"github.com/nervosnetwork/ckb-sdk-go/v2/types"
+	"perun.network/perun-ckb-backend/wallet/address"
 )
+
+type BalanceExtractor func(*indexer.LiveCell) *big.Int
+
+func ckbBalanceExtractor(cell *indexer.LiveCell) *big.Int {
+	return new(big.Int).SetUint64(cell.Output.Capacity)
+}
+
+func sudtBalanceExtractor(cell *indexer.LiveCell) *big.Int {
+	if len(cell.OutputData) != 16 {
+		return big.NewInt(0)
+	}
+	return new(big.Int).SetUint64(binary.LittleEndian.Uint64(cell.OutputData))
+}
 
 func (p *PaymentClient) PollBalances() {
 	defer log.Println("PollBalances: stopped")
@@ -20,7 +36,7 @@ func (p *PaymentClient) PollBalances() {
 		ScriptType:       types.ScriptTypeLock,
 		ScriptSearchMode: types.ScriptSearchModeExact,
 		Filter:           nil,
-		WithData:         false,
+		WithData:         true,
 	}
 	log.Println("PollBalances")
 	updateBalance := func() {
@@ -31,17 +47,24 @@ func (p *PaymentClient) PollBalances() {
 			log.Println("balance poll error: ", err)
 			return
 		}
-		balance := big.NewInt(0)
+		ckbBalance := big.NewInt(0)
+		sudtBalance := big.NewInt(0)
 		for _, cell := range cells.Objects {
-			balance = new(big.Int).Add(balance, new(big.Int).SetUint64(cell.Output.Capacity))
+			ckbBalance = new(big.Int).Add(ckbBalance, ckbBalanceExtractor(cell))
+			sudtBalance = new(big.Int).Add(sudtBalance, sudtBalanceExtractor(cell))
 		}
 
 		p.balanceMutex.Lock()
-		if balance.Cmp(p.balance) != 0 {
-			p.balance = balance
-			bal := p.balance.Int64()
+		if ckbBalance.Cmp(p.balance) != 0 || sudtBalance.Cmp(p.sudtBalance) != 0 {
+			// Update ckb balance.
+			p.balance = ckbBalance
+			ckbBal := p.balance.Int64()
+
+			// Update sudt balance.
+			p.sudtBalance = sudtBalance
+
 			p.balanceMutex.Unlock()
-			p.NotifyAllBalance(bal) // TODO: Update demo tui to allow for big.Int balances
+			p.NotifyAllBalance(ckbBal) // TODO: Update demo tui to allow for big.Int balances
 		} else {
 			p.balanceMutex.Unlock()
 		}
@@ -53,8 +76,10 @@ func (p *PaymentClient) PollBalances() {
 	}
 }
 
-func FormatBalance(bal *big.Int) string {
-	log.Printf("balance: %s", bal.String())
-	balCKByte, _ := ShannonToCKByte(bal).Float64()
-	return strconv.FormatFloat(balCKByte, 'f', 2, 64) + " CKByte"
+func FormatBalance(ckbBal, sudtBal *big.Int) string {
+	log.Printf("balances: ckb = %s || sudt = %s", ckbBal.String(), sudtBal.String())
+	balCKByte, _ := ShannonToCKByte(ckbBal).Float64()
+	return fmt.Sprintf("[green]%s\t[yellow]%s[white]",
+		strconv.FormatFloat(balCKByte, 'f', 2, 64)+" CKByte",
+		fmt.Sprintf("%v", sudtBal.Int64())+" SUDT")
 }
